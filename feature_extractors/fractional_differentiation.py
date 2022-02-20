fc23cb1df22c45141a62c37c5248303f0391efa1
bot <mark.szulyovszky@gmail.com.com>
2022-02-20 11:32:21 +0000

    update

--- feature_extractors/fractional_differentiation.py
+++ feature_extractors/fractional_differentiation.py
@@ -1,8 +1,3 @@
 def feature_fractional_differentiation(df: pd.DataFrame, period: int):
     """No docstring here yet."""
     pass
-
-
-def feature_fractional_differentiation_log(df: pd.DataFrame, period: int):
-    """No docstring here yet."""
-    pass
--- feature_extractors/utils.py
+++ feature_extractors/utils.py
@@ -1,8 +1,3 @@
 def get_close_low_high(df: pd.DataFrame):
     """No docstring here yet."""
     pass
-
-
-def apply_log_if_necessary_series(series: pd.Series, name: str):
-    """No docstring here yet."""
-    pass
